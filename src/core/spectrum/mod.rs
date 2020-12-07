12c85fef146abc57daea892169609c0c1fcd1dd8
Ahmad Kabani <ahmadkabani@yahoo.com>
2020-12-07 18:49:14 -0500

    use compiler feature to switch between rgb and sampled spectrum

--- src/core/spectrum/mod.rs
+++ src/core/spectrum/mod.rs
@@ -16,4 +16,9 @@ pub use rgb_spectrum::*;
 pub use sampled_spectrum::*;
 
 /// Default to using `RGBSpectrum` for rendering.
+#[cfg(not(feature = "sampled-spectrum"))]
 pub type Spectrum = RGBSpectrum;
+
+/// Use `SampledSpectrum` for rendering.
+#[cfg(feature = "sampled-spectrum")]
+pub type Spectrum = SampledSpectrum;
